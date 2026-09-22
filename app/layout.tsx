238c0ceccd82e273b63af285e552c5d5b4a518cf
surajplayr <surajplayr@users.noreply.github.com>
2026-09-22 17:42:18 +0530

    Analytics: Add @vercel/analytics/react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
@@ -32,6 +33,7 @@ export default function RootLayout({
             &copy; {new Date().getFullYear()} ThePrintr. All rights reserved.
           </div>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
